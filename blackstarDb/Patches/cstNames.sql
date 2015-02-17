b2ce84383f3fb1cacf2866fcc38bc8c6eead7d2a
salej1 <salej1@hotmail.com>
2015-02-17 09:20:29 -0600

    Fix Patch cst names

--- blackstarDb/Patches/cstNames.sql
+++ blackstarDb/Patches/cstNames.sql
@@ -1,8 +1,11 @@
 update codexClient set cstId = 2 where sellerName = 'JUAN JOSE ESPINOZA BRAVO' and cstId is null;
 update codexClient set cstId = 4 where sellerName = 'JOSE IVAN MARTIN' and cstId is null;
-update codexClient set cstId = 3 where sellerName = 'FRANCISCO UREÃ‘A' and cstId is null;
+update codexClient set cstId = 3 where sellerName = 'FRANCISCO URE├æA' and cstId is null;
 update codexClient set cstId = 7 where sellerName = 'MICHELLE GALICIA' and cstId is null;
 update codexClient set cstId = 9 where sellerName = 'PILAR PAZ' and cstId is null;
 update codexClient set cstId = 5 where sellerName = 'JUAN RAMOS' and cstId is null;
 update codexClient set cstId = 8 where sellerName = 'DIRECTO' and cstId is null;
+update codexClient set cstId = 6 where sellerName = 'JORGE ALBERTO MARTINEZ' and cstId is null;
+update codexClient set cstId = 11 where sellerName = 'SAUL ANDRADE' and cstId is null;
 update cst set name = 'Francisco Ramón Ureña Villanueva' where cstId = 3;
+
